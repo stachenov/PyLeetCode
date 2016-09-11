80fc47328378834bf2e6f27626b05119cd07f5c6
Sergey A. Tachenov <sergey@tachenov.name>
2016-09-11 16:05:05 +0300

    DP Stefan Pochmann style

--- problems/guess_number_higher_or_lower_2.py
+++ problems/guess_number_higher_or_lower_2.py
@@ -4,21 +4,10 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        # money[size][start] is how much we need to guess a number in the
-        # start (inclusive) ... start + size (exclusive) range
-        money = [[0] * (n + 2)] * 2 # don't need money to guess 0 or 1 number
-
-        def left_part_size(guess, start):
-            return guess - start
-
-        def right_part_size(guess, start, size):
-            return size - left_part_size(guess, start) - 1
-
-        for size in xrange(2, n + 1):
-            money.append([0])
-            max_start = n + 1 - size
-            for start in xrange(1, max_start + 1):
-                money[size].append(min(guess + max(money[left_part_size(guess, start)][start],
-                                                   money[right_part_size(guess, start, size)][guess + 1])
-                                       for guess in xrange(start, start + size)))
-        return money[n][1]
+        money = [[0] * (n + 2) for __ in xrange(n + 2)]
+        for hi in xrange(2, n + 1):
+            for lo in xrange(hi - 1, 0, -1):
+                money[lo][hi] = min(guess + max(money[lo][guess - 1],
+                                                money[guess + 1][hi])
+                                    for guess in xrange(lo, hi + 1))
+        return money[1][n]
