64b7d1827b567b50efebed8937cdcaf239b17c87
Sergey A. Tachenov <sergey@tachenov.name>
2016-09-14 20:07:34 +0300

    376. Wiggle Subsequence

--- problems/wiggle_subsequence.py
+++ problems/wiggle_subsequence.py
@@ -4,3 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        if not nums:
+            return 0
+        peak, valley, peak_len, valley_len = nums[0], nums[0], 1, 1
+        for n in nums[1:]:
+            valley = min(valley, n)
+            peak = max(peak, n)
+            if n > valley and valley_len + 1 > peak_len:
+                peak = n
+                peak_len = valley_len + 1
+            if n < peak and peak_len + 1 > valley_len:
+                valley = n
+                valley_len = peak_len + 1
+        return max(peak_len, valley_len)
