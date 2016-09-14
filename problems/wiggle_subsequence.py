75da8f63c748febf39c9164c06637c45cac841ef
Sergey A. Tachenov <sergey@tachenov.name>
2016-09-14 20:40:29 +0300

    Shorter code

--- problems/wiggle_subsequence.py
+++ problems/wiggle_subsequence.py
@@ -6,14 +6,10 @@ class Solution(object):
         """
         if not nums:
             return 0
-        peak, valley, peak_len, valley_len = nums[0], nums[0], 1, 1
-        for n in nums[1:]:
-            valley = min(valley, n)
-            peak = max(peak, n)
-            if n > valley and valley_len + 1 > peak_len:
-                peak = n
-                peak_len = valley_len + 1
-            if n < peak and peak_len + 1 > valley_len:
-                valley = n
-                valley_len = peak_len + 1
-        return max(peak_len, valley_len)
+        max_len_peak, max_len_valley = 1, 1
+        for n1, n2 in zip(nums, nums[1:]):
+            if n2 > n1:
+                max_len_peak = max(max_len_peak, max_len_valley + 1)
+            elif n2 < n1:
+                max_len_valley = max(max_len_valley, max_len_peak + 1)
+        return max(max_len_peak, max_len_valley)
