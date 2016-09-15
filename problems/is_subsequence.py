74d6257353cd0358fac8e6fdf54a6843bcc12e01
Sergey A. Tachenov <sergey@tachenov.name>
2016-09-15 20:26:40 +0300

    Optimized solution for many strings

--- problems/is_subsequence.py
+++ problems/is_subsequence.py
@@ -1,3 +1,7 @@
+from bisect import bisect_left
+from collections import defaultdict
+
+
 class Solution(object):
     def isSubsequence(self, s, t):
         """
@@ -5,10 +9,15 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        pos_of = defaultdict(list)
+        for it, ct in enumerate(t):
+            pos_of[ct].append(it)
         pos = 0
         for cs in s:
-            pos = t.find(cs, pos)
-            if pos == -1:
+            if cs not in pos_of:
                 return False
-            pos += 1
+            i = bisect_left(pos_of[cs], pos)
+            if i == len(pos_of[cs]):
+                return False
+            pos = pos_of[cs][i] + 1
         return True
