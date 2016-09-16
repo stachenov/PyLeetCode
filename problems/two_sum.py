e31294d680522261914f769a3871461b5045fdaf
Sergey Tachenov <sergey@tachenov.name>
2016-09-16 10:25:55 +0300

    Simplified code

--- problems/two_sum.py
+++ problems/two_sum.py
@@ -8,12 +8,9 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        indices = defaultdict(list)
+        index = {}
         for i, n in enumerate(nums):
-            indices[n].append(i)
-        for n in indices:
             r = target - n
-            if r == n and len(indices[n]) >= 2:
-                return indices[n][:2]
-            elif r in indices:
-                return [indices[n][0], indices[r][0]]
+            if r in index:
+                return [index[r], i]
+            index[n] = i
