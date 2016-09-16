111a28a0d2471857f4716815c26a68642cb55bf4
Sergey Tachenov <sergey@tachenov.name>
2016-09-16 09:37:42 +0300

    Regex solution

--- problems/decode_string.py
+++ problems/decode_string.py
@@ -1,21 +1,14 @@
+import re
+
+
 class Solution(object):
-    def decodeString(self, s, pos=0):
+    def decodeString(self, s):
         """
         :type s: str
         :rtype: str
         """
-        res = ""
-        i = pos
-        while i < len(s):
-            if '0' <= s[i] <= '9':
-                sub_pos = s.index('[', i)
-                rep = int(s[i: sub_pos])
-                dec = self.decodeString(s, sub_pos + 1)
-                res += rep * dec[0]
-                i = dec[1] + 1
-            elif s[i] == ']':
-                return res, i
-            else:
-                res += s[i]
-                i += 1
-        return res
+        # Original idea by @StefanPochmann
+        r = re.compile(r"(\d+)\[([^\d\[\]]*)\]")
+        while r.search(s):
+            s = r.sub(lambda m: int(m.group(1)) * m.group(2), s)
+        return s
