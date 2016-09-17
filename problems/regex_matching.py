2b41c3e05189f8b5bbfc88b680fa1c5a57b25b0d
Sergey A. Tachenov <sergey@tachenov.name>
2016-09-17 17:30:55 +0300

    Memory optimization

--- problems/regex_matching.py
+++ problems/regex_matching.py
@@ -5,15 +5,16 @@ class Solution(object):
         :type p: str
         :rtype: bool
         """
-        match = [[False] * (len(p) + 1) for __ in xrange(len(s) + 1)]
         for i in xrange(len(s) + 1):
+            match = []
             for j in xrange(len(p) + 1):
                 extend_ij, extend_j, extend_i = False, False, False
-                if i > 0 and j > 0 and match[i - 1][j - 1]:
+                if i > 0 and j > 0 and prev[j - 1]:
                     extend_ij = s[i - 1] == p[j - 1] or p[j - 1] == '.'
-                if i > 0 and j > 1 and match[i - 1][j]:
+                if i > 0 and j > 1 and prev[j]:
                     extend_i = p[j - 1] == '*' and (s[i - 1] == p[j - 2] or p[j - 2] == '.')
-                if j > 1 and match[i][j - 2]:
+                if j > 1 and match[j - 2]:
                     extend_j = p[j - 1] == '*'
-                match[i][j] = extend_ij or extend_i or extend_j or i == 0 and j == 0
-        return match[len(s)][len(p)]
+                match.append(extend_ij or extend_i or extend_j or i == 0 and j == 0)
+            prev = match
+        return match[len(p)]
