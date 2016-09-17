e3dba4725b66c70b2f701e0e6cec7062f5b12535
Sergey A. Tachenov <sergey@tachenov.name>
2016-09-17 12:08:12 +0300

    Manacher's algorithm

--- problems/longest_palindromic.py
+++ problems/longest_palindromic.py
@@ -16,9 +16,13 @@ class Solution(object):
                 return posinf
             else:
                 return s[i / 2]
-        p = [0] * modlen
+        p, c = [0] * modlen, 0
         for i in xrange(modlen):
+            if i < c + p[c]:
+                p[i] = min(p[c - (i - c)], c + p[c] - i)
             while get(i - p[i] - 1) == get(i + p[i] + 1):
                 p[i] += 1
+            if i + p[i] > c + p[c]:
+                c = i
         r, c = max((p[i], i) for i in xrange(modlen))
         return s[(c - r) / 2: (c + r - 1) / 2 + 1]
