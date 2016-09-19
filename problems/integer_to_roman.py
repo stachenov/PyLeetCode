a712735ec95320dc3c43720bbfa1b80c0f56adce
Sergey A. Tachenov <sergey@tachenov.name>
2016-09-19 15:45:19 +0300

    Pure data-driven solution

--- problems/integer_to_roman.py
+++ problems/integer_to_roman.py
@@ -5,13 +5,9 @@ class Solution(object):
         :rtype: str
         """
 
-        roman_digits = [(), (1,), (1, 1), (1, 1, 1), (1, 5),
-                        (5,), (5, 1), (5, 1, 1), (5, 1, 1, 1), (1, 10)]
-        letters = [{1: 'I', 5: 'V', 10: 'X'},
-                   {1: 'X', 5: 'L', 10: 'C'},
-                   {1: 'C', 5: 'D', 10: 'M'},
-                   {1: 'M'}]
-        res = ""
-        for pos, digit in enumerate(reversed(map(int, str(num)))):
-            res = ''.join(map(lambda d: letters[pos][d], roman_digits[digit])) + res
-        return res
+        # original idea by @farbizio3
+        M = ["", "M", "MM", "MMM"]
+        C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+        X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+        I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+        return M[num / 1000] + C[num / 100 % 10] + X[num / 10 % 10] + I[num % 10]
