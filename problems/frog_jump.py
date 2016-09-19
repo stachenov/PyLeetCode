0a00ec1b1f288df3eee2797c675d8e7488576b95
Sergey A. Tachenov <sergey@tachenov.name>
2016-09-19 08:38:08 +0300

    Cleaner code

--- problems/frog_jump.py
+++ problems/frog_jump.py
@@ -11,26 +11,24 @@ class Solution(object):
         incoming_jumps = defaultdict(set)
         incoming_jumps[1] = {1}
         for stone in stones[1:]:
-            for jump in incoming_jumps[stone]:
-                for d in xrange(-1 if jump > 1 else 0, +2):
-                    if stone + jump + d in stones_set:
-                        incoming_jumps[stone + jump + d].add(jump + d)
+            for j1 in incoming_jumps[stone]:
+                for j2 in xrange(max(j1 - 1, 1), j1 + 2):
+                    if stone + j2 in stones_set:
+                        incoming_jumps[stone + j2].add(j2)
         return bool(incoming_jumps[stones[-1]])
 
     def canCross_topDown(self, stones):
         # Original idea by @AlgoGuruZ, code mine
-        if stones[1] != 1:
-            return False
         stones_set = set(stones)
         known = {}
         def canCross(s1, s2):
             if (s1, s2) not in known:
                 if s2 == stones[-1]:
-                    known[(s1, s2)] = True
+                    known[s1, s2] = True
                 else:
-                    d = s2 - s1
-                    known[(s1, s2)] = any(canCross(s2, s3) for s3
-                                          in xrange(s2 + d - 1 if d > 1 else s2 + d, s2 + d + 2)
-                                          if s3 in stones_set)
-            return known[(s1, s2)]
-        return canCross(0, 1)
+                    d2 = s2 - s1
+                    known[s1, s2] = any(canCross(s2, s2 + d3) for d3
+                                        in xrange(max(d2 - 1, 1), d2 + 2)
+                                        if s2 + d3 in stones_set)
+            return known[s1, s2]
+        return canCross(0, 0)
