77a696591a76d95603a3904cc8d1fb321643f6be
Sergey A. Tachenov <sergey@tachenov.name>
2016-09-20 09:34:36 +0300

    Better solution

--- problems/threesum.py
+++ problems/threesum.py
@@ -8,17 +8,19 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         nums.sort()
-        num_set = Counter(nums)
-        res = set()
-        for i in xrange(len(nums)):
-            for j in xrange(i + 1, len(nums)):
-                r = 0 - nums[i] - nums[j]
-                if r != nums[i] and r != nums[j] and r in num_set:
-                    res.add(tuple(sorted((nums[i], nums[j], r))))
-                elif r == nums[i] and r == nums[j] and num_set.get(r, 0) >= 3:
-                    res.add((0, 0, 0))
-                elif r == nums[i] and r != nums[j] and num_set.get(r, 0) >= 2:
-                    res.add(tuple(sorted((nums[i], nums[j], r))))
-                elif r != nums[i] and r == nums[j] and num_set.get(r, 0) >= 2:
-                    res.add(tuple(sorted((nums[i], nums[j], r))))
-        return list(map(list, res))
+        res = []
+        for i in xrange(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            r = -nums[i]
+            j, k = i + 1, len(nums) - 1
+            while j < k:
+                while j < k and nums[j] + nums[k] < r:
+                    j += 1
+                while j < k and nums[j] + nums[k] > r:
+                    k -= 1
+                if j < k and nums[j] + nums[k] == r:
+                    res.append([nums[i], nums[j], nums[k]])
+                while j < k and nums[j] + nums[k] == r:
+                    j += 1
+        return res
