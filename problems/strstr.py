e7de602f3c3c6a2deba136b09c280424da9f3831
Sergey A. Tachenov <sergey@tachenov.name>
2016-09-20 17:57:24 +0300

    Cleaner code

--- problems/strstr.py
+++ problems/strstr.py
@@ -8,28 +8,23 @@ class Solution(object):
         if not needle:
             return 0
         kmp = [0] * len(needle)
-        kmp[0] = -1
-        if len(needle) > 1:
-            kmp[1] = 0
         i, j = 0, 1
         while j + 1 < len(needle):
             if needle[j] == needle[i]:
                 kmp[j + 1] = i + 1
                 i, j = i + 1, j + 1
-            elif kmp[i] >= 0:
+            elif i > 0:
                 i = kmp[i]
             else:
-                assert i == 0
                 j += 1
         i, j = 0, 0
-        while i + j < len(haystack):
-            if haystack[i + j] == needle[j]:
-                j += 1
+        while i < len(haystack):
+            if haystack[i] == needle[j]:
+                i, j = i + 1, j + 1
                 if j == len(needle):
-                    return i
-            elif kmp[j] >= 0:
-                i += j - kmp[j]
+                    return i - j
+            elif j > 0:
                 j = kmp[j]
             else:
-                i, j = i + 1, 0
+                i += 1
         return -1
