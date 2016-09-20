ccb46fc1ec55d3b70ddcf07ea6352686bb793997
Sergey A. Tachenov <sergey@tachenov.name>
2016-09-20 09:39:04 +0300

    The same thing, a bit different

--- problems/threesum.py
+++ problems/threesum.py
@@ -9,18 +9,20 @@ class Solution(object):
         """
         nums.sort()
         res = []
-        for i in xrange(len(nums) - 2):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
+        i = 0
+        while i <= len(nums) - 3:
             r = -nums[i]
             j, k = i + 1, len(nums) - 1
             while j < k:
-                while j < k and nums[j] + nums[k] < r:
+                if nums[j] + nums[k] < r:
                     j += 1
-                while j < k and nums[j] + nums[k] > r:
+                elif nums[j] + nums[k] > r:
                     k -= 1
-                if j < k and nums[j] + nums[k] == r:
+                else:
                     res.append([nums[i], nums[j], nums[k]])
-                while j < k and nums[j] + nums[k] == r:
-                    j += 1
+                    while j < k and nums[j] + nums[k] == r:
+                        j += 1
+            while i <= len(nums) - 3 and nums[i] == nums[i + 1]:
+                i += 1
+            i += 1
         return res
