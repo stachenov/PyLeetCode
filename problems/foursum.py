bceeb7008efa979937814748672a0ff08d725db7
Sergey A. Tachenov <sergey@tachenov.name>
2016-09-20 12:13:35 +0300

    O(n^2 lg n) best case solution

--- problems/foursum.py
+++ problems/foursum.py
@@ -1,3 +1,7 @@
+from collections import defaultdict
+from itertools import product
+
+
 class Solution(object):
     def fourSum(self, nums, target):
         """
@@ -5,22 +9,21 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
-        nums.sort()
-        res = []
-        for i in xrange(len(nums) - 3):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            for j in xrange(i + 1, len(nums) - 2):
-                if j > i + 1 and nums[j] == nums[j - 1]:
-                    continue
-                k, m, r = j + 1, len(nums) - 1, target - (nums[i] + nums[j])
-                while k < m:
-                    if nums[k] + nums[m] < r:
-                        k += 1
-                    elif nums[k] + nums[m] > r:
-                        m -= 1
-                    else:
-                        res.append([nums[i], nums[j], nums[k], nums[m]])
-                        while k < m and nums[k] + nums[m] == r:
-                            k += 1
-        return res
+        sums_indexes = defaultdict(list)
+        for i in xrange(len(nums) - 1):
+            for j in xrange(i + 1, len(nums)):
+                sums_indexes[nums[i] + nums[j]].append((i, j))
+        sums = sorted(sums_indexes.keys())
+        res = set()
+        i, j = 0, len(sums) - 1
+        while i <= j:
+            if sums[i] + sums[j] < target:
+                i += 1
+            elif sums[i] + sums[j] > target:
+                j -= 1
+            else:
+                for (i1, j1), (i2, j2) in product(sums_indexes[sums[i]], sums_indexes[sums[j]]):
+                    if i1 != i2 and i1 != j2 and j1 != i2 and j1 != j2:
+                        res.add(tuple(sorted((nums[i1], nums[i2], nums[j1], nums[j2]))))
+                i += 1
+        return list(map(list, res))
--- tests/test_foursum.py
+++ tests/test_foursum.py
@@ -3,6 +3,10 @@ from problems.foursum import Solution
 
 
 @pytest.mark.parametrize("nums,target,expected", [
+    ([0, 0, 0], 0, []),
+    ([0, 0, 0, 0], 0, [
+        [0, 0, 0, 0],
+    ]),
     ([1, 0, -1, 0, -2, 2], 0, [
         [-2, -1, 1, 2],
         [-2, 0, 0, 2],
@@ -10,4 +14,6 @@ from problems.foursum import Solution
     ]),
 ])
 def test_fourSum(nums, target, expected):
-    assert Solution().fourSum(nums, target) == expected
+    quadruples = Solution().fourSum(nums, target)
+    assert len(quadruples) == len(expected)
+    assert set(map(tuple, quadruples)) == set(map(tuple, expected))
