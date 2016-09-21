3077c3739ed2daede395f03c42fba23459dd2ce3
Sergey A. Tachenov <sergey@tachenov.name>
2016-09-21 14:41:18 +0300

    One-pass solution

--- problems/search_in_rotated.py
+++ problems/search_in_rotated.py
@@ -1,6 +1,3 @@
-from bisect import bisect_left
-
-
 class Solution(object):
     def search(self, nums, target):
         """
@@ -8,23 +5,19 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        if not nums:
-            return -1
         lo, hi = 0, len(nums) - 1
-        if nums[0] > nums[-1]:
-            lo = 1
-            while lo <= hi:
-                mid = (lo + hi) / 2
-                if nums[mid] < nums[mid - 1]:
-                    r = mid
-                    break
-                elif nums[0] > nums[mid - 1]:
+        while lo <= hi:
+            mid = (lo + hi) / 2
+            if target == nums[mid]:
+                return mid
+            if nums[0] <= nums[mid]:  # the left part is not rotated
+                if nums[0] <= target <= nums[mid]:
                     hi = mid - 1
                 else:
                     lo = mid + 1
-            if nums[0] <= target <= nums[r - 1]:
-                lo, hi = 0, r - 1
-            else:
-                lo, hi = r, len(nums) -1
-        i = bisect_left(nums, target, lo, hi + 1)
-        return i if i < len(nums) and nums[i] == target else -1
+            else:  # the right part is not rotated
+                if nums[mid] <= target <= nums[-1]:
+                    lo = mid + 1
+                else:
+                    hi = mid - 1
+        return -1
