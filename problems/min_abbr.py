8390b1137324944d1ab88765a3b5ae6892956158
Sergey Tachenov <sergey@tachenov.name>
2016-10-03 13:06:21 +0300

    More edge cases

--- problems/min_abbr.py
+++ problems/min_abbr.py
@@ -17,8 +17,13 @@ class Solution(object):
                 if w[i] == c:  # add to set of words matching target at pos i
                     matches[i].add(w)
         re_num = re.compile(r"(?:\d+|\D+)")
-        if not any(matches):  # we must leave at least one char to distinguish the abbr
+        if not matches[0]:
             return target if len(target) == 1 else target[0] + str(len(target) - 1)
+        if not matches[-1]:
+            return target if len(target) == 1 else str(len(target) - 1) + target[-1]
+        if not all(matches):
+            first_empty = matches.index(set())
+            return str(first_empty) + target[first_empty] + str(len(target) - (first_empty + 1))
 
         def matching(abbr):
             i, s = 0, set()
@@ -42,7 +47,7 @@ class Solution(object):
             else:
                 start = 0 if pos == 0 else pos + 1  # skip 1 char to avoid generating adjacent numbers
                 for i in xrange(start, len(target) - remove):
-                    for r in xrange(1, remove + 1):  # decrease length by r (replace r + 1 chars with 1 number)
+                    for r in xrange(1, remove + 1):  # decrease length by r (replace chars [i:r + 1] with a number)
                         for res in generate(abbr + target[pos:i] + str(r + 1), i + r + 1, remove - r):
                             yield res
 
